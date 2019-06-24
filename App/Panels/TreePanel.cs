d53c97281fd3d7df8de807e0a6091c34ff0ea89a
SillyBits <sillybits@onlinehome.de>
2019-06-24 10:35:43 +0200

    TreePanel.cs: Missing comitting using on System.Text for allow for dumping to use StringBuilder :-/

--- App/Panels/TreePanel.cs
+++ App/Panels/TreePanel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
