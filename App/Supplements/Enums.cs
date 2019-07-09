8f10852c50207e8f3b59936d3b9e1e147edb5dd7
SillyBits <sillybits@onlinehome.de>
2019-07-09 08:52:45 +0200

    Added Supplements/Enums.cs to gather all those enums used

---
+++ App/Supplements/Enums.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+// All the enums used within Satisfactory
+//
+
+namespace SatisfactorySavegameTool.Supplements
+{
+
+	//TODO: Find correct order
+	//TODO: Add suitable tiers to those phases
+	public enum GamePhases
+	{
+		EGP_EarlyGame,
+		EGP_MidGame,
+		EGP_LateGame,
+		EGP_EndGame,
+		EGP_FoodCourt,//???
+		EGP_LaunchTowTruck,//???
+		EGP_Victory,
+		EGP_MAX
+	};
+
+}
