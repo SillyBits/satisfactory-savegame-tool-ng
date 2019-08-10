541780a6330b3c8306184c14499112652e08a1b2
SillyBits <sillybits@onlinehome.de>
2019-08-10 22:05:52 +0200

    CoreLib/Helpers.cs: Added registry helpers

--- CoreLib/Helpers.cs
+++ CoreLib/Helpers.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Win32;
 
 namespace CoreLib
 {
@@ -83,6 +84,55 @@ namespace CoreLib
 
 			return sb.ToString();
 		}
+
+
+		/// <summary>
+		/// Open a subkey in Windows registry.
+		/// </summary>
+		/// <param name="root">Root key to start with (HKLM, HKCU, ...)</param>
+		/// <param name="path">Path to open (separated by '/')</param>
+		/// <param name="writable">Whether or not to open as writable</param>
+		/// <returns>Registry key, or null if not found</returns>
+		public static RegistryKey OpenRegKey(RegistryKey root, string path, bool writable = false)
+		{
+			if (root == null || path == null)
+				return null;
+
+			string[] subs = path.Split('/');
+			return OpenRegKey(root, subs, writable);
+		}
+
+		/// <summary>
+		/// Open a subkey in Windows registry.
+		/// </summary>
+		/// <param name="root">Root key to start with (HKLM, HKCU, ...)</param>
+		/// <param name="path">Array of paths to open</param>
+		/// <param name="writable">Whether or not to open as writable</param>
+		/// <returns>Registry key, or null if not found</returns>
+		public static RegistryKey OpenRegKey(RegistryKey root, string[] path, bool writable = false)
+		{
+			if (root == null || path == null)
+				return null;
+
+			RegistryKey key = root;
+			try
+			{
+				foreach (string sub in path)
+				{
+					key = key.OpenSubKey(sub, writable);
+					if (key == null)
+						break;
+				}
+			}
+			catch
+			{
+				// Access failed, maybe due to missing access restrictions
+				key = null;
+			}
+
+			return key;
+		}
+
 
 	}
 
